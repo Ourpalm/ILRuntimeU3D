572987905f1376b7dee45adde96ac7ebbdcba465
liiir1985 <liiir1985@hotmail.com>
2020-05-01 18:25:04 +0800

    去掉老的绑定方式

--- ILRuntimeDemo/Assets/Samples/ILRuntime/1.6/Demo/Adapters/Editor/ILRuntimeCLRBinding.cs
+++ ILRuntimeDemo/Assets/Samples/ILRuntime/1.6/Demo/Adapters/Editor/ILRuntimeCLRBinding.cs
@@ -7,34 +7,7 @@ using System.Collections.Generic;
 [System.Reflection.Obfuscation(Exclude = true)]
 public class ILRuntimeCLRBinding
 {
-    [MenuItem("ILRuntime/Generate CLR Binding Code")]
-    static void GenerateCLRBinding()
-    {
-        List<Type> types = new List<Type>();
-        types.Add(typeof(int));
-        types.Add(typeof(float));
-        types.Add(typeof(long));
-        types.Add(typeof(object));
-        types.Add(typeof(string));
-        types.Add(typeof(Array));
-        types.Add(typeof(Vector2));
-        types.Add(typeof(Vector3));
-        types.Add(typeof(Quaternion));
-        types.Add(typeof(GameObject));
-        types.Add(typeof(UnityEngine.Object));
-        types.Add(typeof(Transform));
-        types.Add(typeof(RectTransform));
-        types.Add(typeof(CLRBindingTestClass));
-        types.Add(typeof(Time));
-        types.Add(typeof(Debug));
-        //所有DLL内的类型的真实C#类型都是ILTypeInstance
-        types.Add(typeof(List<ILRuntime.Runtime.Intepreter.ILTypeInstance>));
-
-        ILRuntime.Runtime.CLRBinding.BindingCodeGenerator.GenerateBindingCode(types, "Assets/ILRuntime/Generated");
-
-    }
-
-    [MenuItem("ILRuntime/Generate CLR Binding Code by Analysis")]
+   [MenuItem("ILRuntime/Generate CLR Binding Code by Analysis")]
     static void GenerateCLRBindingByAnalysis()
     {
         //用新的分析热更dll调用引用来生成绑定代码
