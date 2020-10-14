7637e6f90185cd767a099b785c59d7c7dec57853
JasonXuDeveloper <jasonxudeveloper@gmail.com>
2020-10-14 13:25:27 +1100

    bug fix

--- ILRuntimeDemo/Assets/Samples/ILRuntime/1.6.5/Demo/Scripts/Examples/08_MonoBehaviour/MonoBehaviourAdapter.cs
+++ ILRuntimeDemo/Assets/Samples/ILRuntime/1.6.5/Demo/Scripts/Examples/08_MonoBehaviour/MonoBehaviourAdapter.cs
@@ -97,7 +97,7 @@ public class MonoBehaviourAdapter : CrossBindingAdaptor
                 mUpdateMethodGot = true;
             }
 
-            if (mStartMethod != null)
+            if (mUpdateMethod != null)
             {
                 appdomain.Invoke(mUpdateMethod, instance, null);
             }
