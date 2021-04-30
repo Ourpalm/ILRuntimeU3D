9c290099d1a2598d16c808e898bfcff79901c6ee
liiir1985 <liiir1985@hotmail.com>
2021-04-30 11:55:21 +0800

    更新运行时到2.0.0-preview.2

--- ILRuntimeDemo/Assets/ILRuntime/Runtime/Intepreter/RegisterVM/JITCompiler.cs
+++ ILRuntimeDemo/Assets/ILRuntime/Runtime/Intepreter/RegisterVM/JITCompiler.cs
@@ -326,7 +326,7 @@ namespace ILRuntime.Runtime.Intepreter.RegisterVM
 #endif
             method.Compiling = false;
             var arr = res.ToArray();
-#if UNITY_EDITOR
+#if DEBUG && !NO_PROFILER
             if (System.Threading.Thread.CurrentThread.ManagedThreadId == method.AppDomain.UnityMainThreadID)
 #if UNITY_5_5_OR_NEWER
                 UnityEngine.Profiling.Profiler.EndSample();
@@ -529,7 +529,13 @@ namespace ILRuntime.Runtime.Intepreter.RegisterVM
                             constrainIdx = lst.Count - 1;
                             hasConstrained = lst[constrainIdx].Code == OpCodeREnum.Constrained;
                         }
-                        if (!canInline || hasConstrained)
+                        bool needInline = canInline && !hasConstrained;
+                        if (needInline)
+                        {
+                            if (toInline.BodyRegister.Length > Optimizer.MaximalInlineInstructionCount / 2)
+                                needInline = false;
+                        }
+                        if (!needInline)
                         {
                             if (code.Code == Code.Callvirt && m is ILMethod)
                             {
@@ -603,7 +609,7 @@ namespace ILRuntime.Runtime.Intepreter.RegisterVM
 
 #endif
                             Optimizer.InlineMethod(block, toInline, link, ref jumptables, baseRegIdx, hasRet);
-#if UNITY_EDITOR
+#if DEBUG && !NO_PROFILER
             if (System.Threading.Thread.CurrentThread.ManagedThreadId == method.AppDomain.UnityMainThreadID)
 #if UNITY_5_5_OR_NEWER
                 UnityEngine.Profiling.Profiler.EndSample();
