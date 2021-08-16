d14b67b771b5b05df9d392d45b9d5d37eaa31218
Park June Chul <pjc0247@naver.com>
2021-08-16 15:46:40 +0900

    fix: make MonoBehaviourAdapterEditor recognize more types

--- ILRuntimeDemo/Assets/Samples/ILRuntime/1.6.7/Demo/Scripts/Examples/08_MonoBehaviour/Editor/MonoBehaviourAdapterEditor.cs
+++ ILRuntimeDemo/Assets/Samples/ILRuntime/1.6.7/Demo/Scripts/Examples/08_MonoBehaviour/Editor/MonoBehaviourAdapterEditor.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEditor;
 using System.Collections;
 using System.Collections.Generic;
@@ -21,7 +21,7 @@ public class MonoBehaviourAdapterEditor : UnityEditor.UI.GraphicEditor
         if (instance != null)
         {
             EditorGUILayout.LabelField("Script", clr.ILInstance.Type.FullName);
-            
+
             int index = 0;
             foreach (var i in instance.Type.FieldMapping)
             {
@@ -29,7 +29,7 @@ public class MonoBehaviourAdapterEditor : UnityEditor.UI.GraphicEditor
                 var name = i.Key;
                 var type = instance.Type.FieldTypes[index];//在这里不能用i.Value，因为Unity有HideInInspector方法，隐藏序列化的值，但是还是会被计数
                 index++;
-                
+
                 var cType = type.TypeForCLR;
                 if (cType.IsPrimitive)//如果是基础类型
                 {
@@ -43,7 +43,31 @@ public class MonoBehaviourAdapterEditor : UnityEditor.UI.GraphicEditor
                 else
                 {
                     object obj = instance[i.Value];
-                    if (typeof(UnityEngine.Object).IsAssignableFrom(cType))
+                    if (cType == typeof(Vector2))
+                    {
+                        instance[i.Value] = EditorGUILayout.Vector2Field(name, (Vector2)instance[i.Value]);
+                    }
+                    else if (cType == typeof(Vector3))
+                    {
+                        instance[i.Value] = EditorGUILayout.Vector3Field(name, (Vector3)instance[i.Value]);
+                    }
+                    else if (cType == typeof(Vector4))
+                    {
+                        instance[i.Value] = EditorGUILayout.Vector4Field(name, (Vector4)instance[i.Value]);
+                    }
+                    else if (cType == typeof(Color))
+                    {
+                        instance[i.Value] = EditorGUILayout.ColorField(name, (Color)instance[i.Value]);
+                    }
+                    else if (cType == typeof(Bounds))
+                    {
+                        instance[i.Value] = EditorGUILayout.BoundsField(name, (Bounds)instance[i.Value]);
+                    }
+                    else if (cType == typeof(AnimationCurve))
+                    {
+                        instance[i.Value] = EditorGUILayout.CurveField(name, (AnimationCurve)instance[i.Value]);
+                    }
+                    else if (typeof(UnityEngine.Object).IsAssignableFrom(cType))
                     {
                         //处理Unity类型
                         var res = EditorGUILayout.ObjectField(name, obj as UnityEngine.Object, cType, true);
